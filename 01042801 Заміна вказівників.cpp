d58ef5830f409ee5ebb2bbcedeb005fd6e9c9786
Anastasiia Fastovets <afastovets@gmail.com>
2023-12-01 20:18:35 +0200

    Add project files.

---
+++ 01042801 Заміна вказівників.cpp
@@ -0,0 +1,38 @@
+﻿/*
+Змініть наступну програму, замінивши звичайний вказівник на розумний вказівник std::unique_ptr.
+*/
+
+#include <iostream>
+#include <memory>
+
+class Fraction
+{
+private:
+	int m_numerator = 0;
+	int m_denominator = 1;
+
+public:
+	Fraction(int numerator = 0, int denominator = 1) :
+		m_numerator(numerator), m_denominator(denominator) {}
+
+	friend std::ostream& operator<<(std::ostream& out, const Fraction& f1)
+	{
+		out << f1.m_numerator << "/" << f1.m_denominator;
+		return out;
+	}
+};
+
+void printFraction(const Fraction* const ptr)
+{
+	if (ptr)
+		std::cout << *ptr;
+}
+
+int main()
+{
+	std::unique_ptr<Fraction> ptr(new Fraction(49, 11711));
+
+	printFraction(ptr.get());
+
+	return 0;
+}
